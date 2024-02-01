c8b59dcf5fa3f01d1dbddbdf64fb29ffb47a5921
duboye <ogunduboyeolawale@gmail.com>
2024-02-01 08:45:59 -0800

    navigation smooth scrolling functionality

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
+'use strict'
 const scrollToBtn = document.querySelector(".btn");
-const infoSection = document.querySelector("#more_section");
+const infoSection = document.querySelector("#service__4");
 
 scrollToBtn.addEventListener("click", function (e) {
   const sectionLocation = infoSection.getBoundingClientRect();
@@ -10,3 +11,14 @@ scrollToBtn.addEventListener("click", function (e) {
     behavior: "smooth"
   });
 });
+
+// add event listner to a parent element
+const navLinkUL = document.querySelector('.nav__links')
+navLinkUL.addEventListener('click', function(event){
+  event.preventDefault()
+
+  if(event.target.classList.contains('nav__link')){
+    const id = event.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
