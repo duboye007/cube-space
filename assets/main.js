e7c250d386d1ecb3d20a92ffa5deb7969ada6a4a
duboye <ogunduboyeolawale@gmail.com>
2024-02-15 06:33:55 -0800

    slider completed

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,13 @@
 "use strict";
 const scrollToBtn = document.querySelector(".btn");
 const infoSection = document.querySelector("#service__4");
+const navLinkUL = document.querySelector(".nav__links");
+const sectionsEl = document.querySelectorAll("section");
+const slideEl = document.querySelectorAll(".slide");
+const dotsEl = document.querySelector(".dots");
+const sliderBtnRight = document.querySelector(".slider__btn--right");
+const sliderBtnLeft = document.querySelector(".slider__btn--left");
+
 
 scrollToBtn.addEventListener("click", function (e) {
   const sectionLocation = infoSection.getBoundingClientRect();
@@ -13,7 +20,7 @@ scrollToBtn.addEventListener("click", function (e) {
 });
 
 // add event listner to a parent element
-const navLinkUL = document.querySelector(".nav__links");
+
 navLinkUL.addEventListener("click", function (event) {
   event.preventDefault();
 
@@ -23,16 +30,13 @@ navLinkUL.addEventListener("click", function (event) {
   }
 });
 
-// add hidden class to all sections
-const sectionsEl = document.querySelectorAll("section");
 
 // reveal section upon scrolling
 const showSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section__hidden");
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 const sectionObserver = new IntersectionObserver(showSection, {
   root: null,
@@ -40,6 +44,91 @@ const sectionObserver = new IntersectionObserver(showSection, {
 });
 //  add the class hiden to sections
 sectionsEl.forEach((section) => {
-  section.classList.add("section__hidden");
+  // section.classList.add("section__hidden");
   sectionObserver.observe(section);
 });
+
+// function create dots elements
+const createDots = function () {
+  slideEl.forEach((_, index) => {
+    dotsEl.insertAdjacentHTML("beforeend", `<button class="dots__dot" data-slide="${index}"></button>`)
+  });
+};
+
+const init = function (){
+  createDots();
+  goToSlide(currSlide);
+  activateDots(currSlide);
+}
+
+
+let currSlide = 0
+const maxSlide = slideEl.length - 1
+
+// slide action
+const goToSlide = function(slide) {
+slideEl.forEach((slideEl, index) => {
+  slideEl.style.transform = `translateX(${100 * (index - slide)}%)`;
+});
+}
+
+const activateDots = function (slide) {
+  document.querySelectorAll(".dots__dot").forEach((dot) => {
+    dot.classList.remove("dots__dot--active");
+  });
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+
+
+
+// moving slides to the right
+const slideRight = function() {
+  if (currSlide === maxSlide) {
+    currSlide = 0;
+  } else {
+    currSlide++;
+  }
+  goToSlide(currSlide);
+  activateDots(currSlide)
+}
+
+// moving slides to the left
+const slideLeft = function() {
+  if(currSlide === 0){
+    currSlide = maxSlide
+  } else {
+    currSlide--;
+  }
+  goToSlide(currSlide);
+  activateDots(currSlide)
+}
+
+init();
+sliderBtnRight.addEventListener("click", slideRight);
+sliderBtnLeft.addEventListener("click", slideLeft);
+
+// when we use the arrow keys on the keyboard
+document.addEventListener('keydown', function(event) {
+  event.key === 'ArrowRight' && slideRight()
+
+  event.key === 'ArrowLeft' && slideLeft()
+
+})
+
+// dots action for slide
+dotsEl.addEventListener('click', function(event) {
+  if(event.target.classList.contains("dots__dot")){
+    const {slide} = event.target.dataset
+    goToSlide(slide)
+    activateDots(slide)
+  }
+
+})
+
+
+
+
+
